e174781996a64d5ee00b1b411d7ffe9089352eee
taku-mh <mhuru.tm@gmail.com>
2021-08-13 03:12:56 -0400

    attempt to run on terminal

---
+++ common.js
@@ -0,0 +1,35 @@
+const app = express()
+const port = 3000
+app.use(express.json({ limit: 500000}));
+
+app.use(express.static('public'))
+
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+// // Get all the tabs
+// const tabs = document.querySelectorAll('.tab');
+
+
+// tabs.forEach(clickedTab => {
+//     // Add onClick event listener on each tab
+//     clickedTab.addEventListener('click', () => {
+//         // Remove the active class from all the tabs (this acts as a "hard" reset)
+//         tabs.forEach(tab => {
+//             tab.classList.remove('active');
+//         });
+
+//         // Add the active class on the clicked tab
+//         clickedTab.classList.add('active');
+//     });
+// });
