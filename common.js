e2a6ffd35ae9411de49a2e886635f344bd2d857f
taku-mh <mhuru.tm@gmail.com>
2021-08-17 19:12:18 -0400

    marks recommendations

--- common.js
+++ common.js
@@ -1,19 +1,12 @@
-const app = express()
-const port = 3000
-app.use(express.json({ limit: 500000}));
+// const app = express()
+// const port = 3000
+// app.use(express.json({ limit: 500000}));
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-})
-
-
-
-
-
-
-
+// app.listen(port, () => {
+//     console.log(`app listening at http://localhost:${port}`)
+// })
 
 
 
